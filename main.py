a56464e4752fa82a11fde7d52ddc139da58115b4
sxkaya <sxkaya@example.com>
2026-01-31 02:27:56 +0300

    site guncellendi

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles # İleride resim/css eklersen lazım olur
 
 app = FastAPI()
 
+# Ana sayfaya (/) girildiğinde index.html dosyasını gönderir
 @app.get("/")
 def ana_sayfa():
     return FileResponse("index.html")
 
-@app.get("/api/selam")
-def selamlama():
-    return {"status": "runing", "message": "selam!", "version": "v1"}
+@app.get("/.well-known/discord")
+def dc_dogrulama():
+    return "dh=83c3b01a0d3112bb962651a1cde20350bffb5032"
+ 
+@app.get("/api/v1/selam")
+def selam_api():
+    return {
+        "durum": "basarili",
+        "mesaj": "Vercel üzerinden selamlar!",
+        "versiyon": "1.0"
+    }
